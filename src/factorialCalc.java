f3be6ca677a1d92ed212f6a7a0af2aee9d831549
Jdon5 <162381746+Jdon5@users.noreply.github.com>
2024-12-28 17:48:16 +0100

    exercise1 factorial  calculation

--- src/factorialCalc.java
+++ src/factorialCalc.java
@@ -1,4 +1,13 @@
 public class factorialCalc {
-    // recursive factorial calculation
+    // recursive factorial calculation  exercise1
     
+    static int factorial(int num){
+        if(num == 0) return 1;
+        else 
+        return factorial(num-1)*num;
+    }
+    public static void main(String[] args) {
+        
+        System.out.println(factorial(5));
+    }
 }
