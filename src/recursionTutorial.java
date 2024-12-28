97a299c48067bdcc14d3a9c8cfd4ff82c76e4d10
Jdon5 <162381746+Jdon5@users.noreply.github.com>
2024-12-28 15:36:47 +0100

    added factorial recrusion

--- src/recursionTutorial.java
+++ src/recursionTutorial.java
@@ -1,3 +1,23 @@
 public class recursionTutorial {
-    
+
+    static void walk(int steps){
+        if(steps < 1) // base case
+        return; 
+        else
+        System.out.println("You take a step!");
+        walk(steps-1); // recursive 
+    }
+
+    static int factorial(int num){
+        if(num<1) return 1;// base case
+        return num* factorial(num-1);//recursive
+    }
+
+
+    public static void main(String[] args) {
+        //walk(5);
+        System.out.println(factorial(5));
+        
+    }
 }
+
