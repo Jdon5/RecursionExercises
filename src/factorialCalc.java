2dddea4ea99e48282e07732ef8e616adcbeace43
Jdon5 <162381746+Jdon5@users.noreply.github.com>
2024-12-28 17:55:34 +0100

    fix factorialCalc for positive integers

--- src/factorialCalc.java
+++ src/factorialCalc.java
@@ -2,7 +2,7 @@ public class factorialCalc {
     // recursive factorial calculation  exercise1
     
     static int factorial(int num){
-        if(num == 0) return 1;
+        if(num <= 0) return 1;
         else 
         return factorial(num-1)*num;
     }
